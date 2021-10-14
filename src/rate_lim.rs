f6c9d019770b94a003dd05c593642e754a4c3c3c
f3kilo <f3kilo@yandex.ru>
2021-10-14 18:03:56 +0300

    Temporary sync rate limiter.

--- src/rate_lim.rs
+++ src/rate_lim.rs
@@ -31,7 +31,6 @@ where
     type Future = futures::future::Ready<Result<Self::Transform, Self::InitError>>;
 
     fn new_transform(&self, service: S) -> Self::Future {
-        log::trace!("new_transform(&self, service: S) called");
         let mw = RateLimiterMiddleware::new(self.connection_manager.clone(), service);
         futures::future::ready(Ok(mw))
     }
@@ -63,10 +62,13 @@ where
     actix_web::dev::forward_ready!(service);
 
     fn call(&self, req: ServiceRequest) -> Self::Future {
-        log::trace!("call(&self, req: ServiceRequest) called");
-        let addr = req.peer_addr();
+        let address = req.peer_addr();
         let fut = self.service.call(req);
-        RateLimiterFuture::new(fut, addr, self.connection_manager.clone())
+        let connections_result = futures::executor::block_on(get_connections_count(
+            address,
+            self.connection_manager.clone(),
+        ));
+        RateLimiterFuture::new(fut, connections_result)
     }
 }
 
@@ -79,8 +81,7 @@ where
     #[pin]
     fut: S::Future,
     _phantom: PhantomData<B>,
-    addr: Option<SocketAddr>,
-    cm: ConnectionManager,
+    connections_result: CustomResult<u64>,
 }
 
 impl<S, B> RateLimiterFuture<S, B>
@@ -88,14 +89,13 @@ where
     S: Service<ServiceRequest, Response = ServiceResponse<B>, Error = actix_web::Error>,
     B: MessageBody,
 {
-    const CONNECTIONS_LIMIT: u64 = 500;
+    const CONNECTIONS_LIMIT: u64 = 5;
 
-    pub fn new(fut: S::Future, addr: Option<SocketAddr>, cm: ConnectionManager) -> Self {
+    pub fn new(fut: S::Future, connections_result: CustomResult<u64>) -> Self {
         Self {
             fut,
             _phantom: Default::default(),
-            addr,
-            cm,
+            connections_result,
         }
     }
 }
@@ -108,27 +108,18 @@ where
     type Output = Result<ServiceResponse<B>, actix_web::Error>;
 
     fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
-        log::trace!("poll Rate lim called");
-
-        let connects =
-            futures::executor::block_on(get_connections_count(self.addr, self.cm.clone()));
-        let requests_count = match connects {
-            Ok(reqs) => reqs,
-            Err(e) => return Poll::Ready(Err(e.into())),
+        let connections = match &self.connections_result {
+            Ok(c) => *c,
+            Err(e) => return Poll::Ready(Err(e.clone().into())),
         };
 
-        log::trace!("connections count future finished");
-
-        if requests_count > Self::CONNECTIONS_LIMIT {
-            log::info!("Too many requests. Connection dropped.");
+        if connections > Self::CONNECTIONS_LIMIT {
             return Poll::Ready(Err(CustomError::TooManyRequests {
-                actual: requests_count,
+                actual: connections,
                 max: Self::CONNECTIONS_LIMIT,
             }
             .into()));
         }
-
-        log::trace!("Requests count check passed.");
 
         let this = self.project();
         let res = match futures::ready!(this.fut.poll(cx)) {
@@ -142,30 +133,23 @@ where
 const KEY_PREFIX: &str = "RATE_LIMIT_";
 
 async fn get_connections_count(
-    addr: Option<SocketAddr>,
+    address: Option<SocketAddr>,
     mut cm: ConnectionManager,
 ) -> CustomResult<u64> {
-    let addr = addr.ok_or_else(|| CustomError::InternalError("Can't parse peer address".into()))?;
+    let addr =
+        address.ok_or_else(|| CustomError::InternalError("Can't parse peer address".into()))?;
 
     let current_minute = Utc::now().minute();
     let key = format!("{}:{}:{}", KEY_PREFIX, addr, current_minute);
 
     let mut pipeline = redis::pipe();
 
-    log::trace!("connections count request starting");
-
-    let (connections_count, other) = pipeline
+    let (connections_count, _) = pipeline
         .atomic()
         .incr(&key, 1)
         .expire(&key, 60)
         .query_async::<_, (u64, u64)>(&mut cm)
         .await?;
-
-    log::trace!(
-        "connections count request finished: {}, {}",
-        connections_count,
-        other
-    );
 
     Ok(connections_count)
 }
